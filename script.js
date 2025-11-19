3ee1e086e57440dc22d5080501e948f939500001
Mereticks Dev <118029848+Mereticks-Dev@users.noreply.github.com>
2025-11-19 10:46:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,4 @@
 /* =========================================
-   1. FUNÇÕES DE INTERFACE E WHATSAPP
-   ========================================= */
-function toggleMenu() {
-    const nav = document.getElementById('nav-links');
-    if (nav) nav.classList.toggle('active');
-}
-
-function handleWhatsAppSubmit(e) {
-    e.preventDefault();
-    const name = document.getElementById('name').value;
-    const service = document.getElementById('service-type').value;
-    const guests = document.getElementById('guests').value;
-    const date = document.getElementById('date').value;
-    const message = document.getElementById('message').value;
-    
-    let dataFormatada = date;
-    if(date) {
-        const parts = date.split('-'); 
-        dataFormatada = `${parts[2]}/${parts[1]}/${parts[0]}`;
-    }
-
-    const text = `*Olá Brot Pão! Orçamento via Site.*%0A%0A` +
-                 `*Nome:* ${name}%0A` +
-                 `*Interesse:* ${service}%0A` +
-                 `*Convidados:* ${guests || 'Não informado'}%0A` +
-                 `*Data:* ${dataFormatada || 'A definir'}%0A` +
-                 `*Obs:* ${message || '-'}`;
-
-    const phone = "554196050977"; 
-    window.open(`https://wa.me/${phone}?text=${text}`, '_blank');
-
-}
-
-/* =========================================
    PROTEÇÃO BÁSICA (ANTI-CÓPIA)
    ========================================= */
 document.addEventListener('contextmenu', event => event.preventDefault());
@@ -51,3 +17,4 @@ document.onkeydown = function(e) {
         return false;
     }
 }
+
